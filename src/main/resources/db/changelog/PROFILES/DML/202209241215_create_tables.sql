37d309f8d8db2630a0ddc3a9862148f267d253a3
Nikolay Polyakov <nikolay.polyakov@ormco.com>
2022-09-24 13:19:28 +0300

    initial commit

---
+++ src/main/resources/db/changelog/PROFILES/DML/202209241215_create_tables.sql
@@ -0,0 +1,21 @@
+-- liquibase formatted sql
+-- changeset npoliakoff:202209241215
+CREATE TABLE user
+(
+    id       INT PRIMARY KEY,
+    email    VARCHAR(50),
+    password VARCHAR(250)
+);
+
+
+CREATE TABLE profile
+(
+    id         INT PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name  VARCHAR(50),
+    age        INTEGER,
+    interests  VARCHAR(250),
+    city       VARCHAR(50),
+    owner_id   INT,
+    CONSTRAINT FK_OwnerOfProfile FOREIGN KEY (owner_id) REFERENCES user (id)
+);
